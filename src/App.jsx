b4c8de208ccf47d8a41e5a87335f29f24fcf76bf
Laud Tetteh <kobbykurtx@gmail.com>
2025-02-08 17:49:52 +0000

    installed react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
+import Users from "./pages/Users";
+import GlobalStyles from "./styles/GlobalStyle";
+import AppLayout from "./ui/AppLayout";
+
 function App() {
-  return <div>Hey, is this the boy we are expecting for the interview</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="account" element={<Account />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
